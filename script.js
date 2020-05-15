71be51d672fc9fd42a6e80479298e53efe7e1b83
iuriev <ivan.iuriev@gmail.com>
2020-05-15 16:37:00 +0300

    initial commit

--- script.js
+++ script.js
@@ -10,8 +10,6 @@ let takeProfit3;
 let fullLink;
 
 window.addEventListener("load", function () {
-
-
     messageText = document.getElementById('message');
     brand = document.getElementById('brand');
     signalType = document.getElementById('signalType');
@@ -22,30 +20,26 @@ window.addEventListener("load", function () {
     takeProfit2 = document.getElementById('takeProfit2');
     takeProfit3 = document.getElementById('takeProfit3');
     fullLink = document.getElementById('fullLink');
-
-
     document.getElementById('generate').addEventListener('click', createLink);
     parseInstruments();
-
 });
 
 let createLink = function () {
-    let link = `https://${brand.value}/?isSMSNotify
-    &type="${signalType.value}"
-    &symbol="${instrument.value}"
-    &direction="${orderDirection.value}"
-    &sl="${stopLoss.value}"
-    &tp1="${takeProfit1.value}"
-    &tp2="${takeProfit2.value}"
-    &tp3="${takeProfit3.value}"
+    let link = `https://${brand.value}/?isSmsNotify
+    &type=${signalType.value}
+    &symbol=${instrument.value}
+    &direction=${orderDirection.value}
+    &sl=${stopLoss.value}
+    &tp1=${takeProfit1.value}
+    &tp2=${takeProfit2.value}
+    &tp3=${takeProfit3.value}
     `;
     link = link.replace(/\s{2,}/g, '');
-    console.log(link)
     fullLink.value = link;
 }
 
 function readTextFile(file, callback) {
-    var rawFile = new XMLHttpRequest();
+    let rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
     rawFile.onreadystatechange = function () {
@@ -66,7 +60,6 @@ let parseInstruments = function () {
             instrument.appendChild(opt);
         }
     });
-
 };
 
 
